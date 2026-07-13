f6d949437d0699a6b0102bdaabba841043caf872
shristi mishra <mshristi2001@gmail.com>
2026-07-14 00:52:20 +0530

    Fix Express 5 wildcard route syntax

--- server.js
+++ server.js
@@ -2,11 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve the React build folder as static files
 app.use(express.static(path.join(__dirname, 'build')));
 
-// Catch-all: send index.html for any route (needed for React Router, if you use it)
-app.get('*', (req, res) => {
+app.get('/*splat', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
